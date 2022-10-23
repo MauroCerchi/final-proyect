05e15ae7b09331eac08d4e7590234a11a6878e94
Mauro Cerchi <holasoyelbola@gmail.com>
2022-10-23 16:23:53 -0300

    Clase usuaruio

--- proyectofinal/models.py
+++ proyectofinal/models.py
@@ -1,3 +1,10 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+
+class usuario(models.Model):
+    nombre_usuario = models.CharField(max_length=100)
+    email_usuario = models.CharField(max_lenght=100)
+    pass_usuario = models.CharField(min_lenght=6, max_lenght=20)
+    
